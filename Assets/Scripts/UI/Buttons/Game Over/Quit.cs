3b64928dd3fd1485b2257d7540ea25b0e5783e9b
Patrick <boyscoutpatrick813@gmail.com>
2020-02-27 14:15:32 -0600

    Added the Death scene with UI

---
+++ Assets/Scripts/UI/Buttons/Game Over/Quit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		if (Application.isEditor)
+        {
+			Debug.Break();
+        }
+		else
+        {
+			Application.Quit();
+        }
+	}
+}
